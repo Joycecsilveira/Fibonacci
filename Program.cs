6746769a04861d43996dc8b77f79d0a4d65250f7
Joyce Silveira da Costa <80860874+Joycecsilveira@users.noreply.github.com>
2024-11-11 16:37:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculaFibonacci(0, 1, 1, 40);
+            Console.ReadKey();
+        }
+        private static void CalculaFibonacci(int a, int b, int counter, int len)
+        {
+            if (counter <= len)
+            {
+                Console.Write("{0} ", a);
+                CalculaFibonacci(b, a + b, counter + 1, len++);
+            }
+        }
+
+    }
+}
